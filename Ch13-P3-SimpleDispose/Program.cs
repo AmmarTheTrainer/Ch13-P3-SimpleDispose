11ebfbb9a95f0ba50b8bb364f03d665c9a3cef35
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-11-08 18:29:14 +0500

    final update from head office

--- Ch13-P3-SimpleDispose/Program.cs
+++ Ch13-P3-SimpleDispose/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,52 @@ namespace Ch13_P3_SimpleDispose
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Dispose *****\n");
+            //// Create a disposable object and call Dispose() to free any internal resources.
+            //MyResourceWrapper rw = new MyResourceWrapper();
 
+            //if (rw is IDisposable)
+            //    rw.Dispose();
+
+            #region C# using keyword
+
+            //MyResourceWrapper rw = new MyResourceWrapper();
+            //try
+            //{
+            //    // Use the members of rw.
+            //}
+            //finally
+            //{
+            //    // Always call Dispose(), error or not.
+            //    rw.Dispose();
+            //}
+
+            //// Dispose() is called automatically when the using scope exits.
+            //using (MyResourceWrapper rw = new MyResourceWrapper())
+            //{
+            //    // Use rw object.
+            //}
+
+
+            // Use a comma-delimited list to declare multiple objects to dispose.
+            using (MyResourceWrapper rw = new MyResourceWrapper(), rw2 = new MyResourceWrapper())
+            {
+                // Use rw and rw2 objects.
+            }
+
+            #endregion
+
+            Console.ReadLine();
+        }
+        // Assume you have imported
+        // the System.IO namespace...
+        static void DisposeFileStream()
+        {
+            FileStream fs = new FileStream("myFile.txt", FileMode.OpenOrCreate);
+            // Confusing, to say the least!
+            // These method calls do the same thing!
+            fs.Close();
+            fs.Dispose();
         }
     }
 }
